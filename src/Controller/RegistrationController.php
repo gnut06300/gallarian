505d672fb204e7635d6f292b0acb41d6bd8c46d2
Jean-Philippe <jp.desmet1@gmail.com>
2021-04-01 15:49:55 +0200

    mail de confirmation + boutons menu et redirection logout

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -77,13 +77,25 @@ class RegistrationController extends AbstractController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
+        //Récupération de l'utilisateur
+        $user = $this->getUser();
+
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
+             // generate a signed url and email it to the user
+             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+             (new TemplatedEmail())
+                 ->from(new Address('test@test.fr', 'test'))
+                 ->to($user->getEmail())
+                 ->subject('Please Confirm your Email')
+                 ->htmlTemplate('registration/confirmation_email.html.twig')
+         );
+            //$this->addFlash('verify_email_error', $exception->getReason());
+            $this->addFlash('success', "Un nouvel email de vérification vous a été envoyé.");
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('categories');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
